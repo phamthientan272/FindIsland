7ced99c462449a384f33e186ed12a85ad8093c3c
phamthientan <thientan.pham@anacle.com>
2023-02-09 16:45:12 +0800

    Change from dfs to bfs

--- app/algo/island_counter.py
+++ app/algo/island_counter.py
@@ -1,6 +1,6 @@
 import sys
 from utils.recursion_limit import recursionlimit
-
+from collections import deque
 class Map:
     def __init__(self, map):
         self.__map = map
@@ -14,9 +14,9 @@ class Map:
         
         default_recursion_limit = sys.getrecursionlimit()
         if self.__n_col * self.__n_row >= default_recursion_limit:
-            limit = self.__n_col * self.__n_row * 2
+            limit = self.__n_col * self.__n_row * 4
         else:
-            limit = default_recursion_limit
+            limit = default_recursion_limit * 2
         print(f"the recursion limit is {limit}")
            
         with recursionlimit(limit):
@@ -24,7 +24,8 @@ class Map:
                 for j in range(self.__n_col):
                     if visited[i][j] == False and self.__map[i][j] == 1:
                         self.__count += 1
-                        self.__dfs(i, j, visited)
+                        # self.__dfs(i, j, visited)
+                        self.__bfs(i, j, visited)
             return self.__count 
         
         
@@ -42,7 +43,23 @@ class Map:
             if self.__is_safe_to_visit(i + row_index[k], j + col_index[k], visited):
                 self.__dfs(i + row_index[k], j + col_index[k], visited)
     
-    
+    def __bfs(self, i, j, visited):
+        """
+        Perform BFS on the map to traverse the connected cells.
+        """
+        queue = deque([(i, j)])
+        visited[i][j] = True
+        
+        row_index = [-1, -1, -1, 0, 0, 1, 1, 1]
+        col_index = [1, 0, 1, -1, 1, -1, 0, 1]
+        
+        while queue:
+            i, j = queue.popleft()
+            for k in range(8):
+                if self.__is_safe_to_visit(i + row_index[k], j + col_index[k], visited):
+                    visited[i + row_index[k]][j + col_index[k]] = True
+                    queue.append((i + row_index[k], j + col_index[k]))
+                    
     def __is_safe_to_visit(self, i, j, visited):
         """
         A cell that is available to call a DFS is its position is inside the map,
--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from algo.island_counter import Map
 from utils.file_reader import read_map_from_file
 
 if __name__ == "__main__":
-    map = read_map_from_file("../tests/map_extreme_10K.txt")
+    map = read_map_from_file("../tests/map_extreme_10M.txt")
     print(len(map), len(map[0]), len(map)*len(map[0]))
     m = Map(map)
     n_islands = m.count_islands()
